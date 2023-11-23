0c91faec9f6c33ae2669e6b4e57d0d2e1a6a3924
tama1305 <76451037+tama1305@users.noreply.github.com>
2023-11-23 11:37:00 +0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hasil Analisi data interaktif")
+panjang = st.number_input ("Masukan pertanyaan 1")
+panjang = st.number_input ("Masukan pertanyaan 2")
+sumbit = st.button ("sumbit")
